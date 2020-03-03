188e9eb866005cbfb474daca7f96f953cfd8aaaf
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-03 16:22:30 -0300

    DONE 2

--- desafios/desafio1.sql
+++ desafios/desafio1.sql
@@ -1,7 +1,7 @@
 SELECT
     COUNTRY_NAME,
     CASE
-        WHEN REGION_ID = 1 THEN 'Incluído'
+        WHEN (REGION_ID = 1) THEN 'Incluído'
         ELSE 'Não incluído'
     END AS 'Status Inclusão'
 FROM hr.countries LIMIT 25;
