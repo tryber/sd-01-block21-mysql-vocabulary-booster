9a2b1b82303ce1f4c4f1cd61a039fa3011725abb
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-03 17:22:45 -0300

    DESAFIO 3

---
+++ desafios/desafio3.sql
@@ -0,0 +1,5 @@
+SELECT JOB_TITLE, AVG(MAX_SALARY - MIN_SALARY) AS 'Diferença média entre salários mínimos e máximos'
+FROM hr.jobs
+GROUP BY JOB_TITLE
+ORDER BY AVG(MAX_SALARY - MIN_SALARY)
+LIMIT 19;
