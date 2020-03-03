1ce1764778eb6539c086133bac03d8011877a782
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-03-03 12:09:03 -0300

    requisito 12 finalizado

---
+++ challengesSQL/desafio11.sql
@@ -0,0 +1,5 @@
+SELECT CustomerID, CustomerName, Country AS País, COUNT(*) AS Número_de_Compatriotas
+FROM w3schools.customers
+GROUP BY Country
+HAVING Número_de_Compatriotas > 1
+ORDER BY ContactName ASC;
--- challengesSQL/desafio8.sql
+++ challengesSQL/desafio8.sql
@@ -4,5 +4,3 @@ INNER JOIN w3schools.customers AS c
 ON o.CustomerID = c.CustomerID
 WHERE o.ShipperID = 1 OR o.ShipperID = 2
 ORDER BY 2 ASC; 
-
---CONFERIR COM O CACIQUE A QUANTIDADE DE RESULTADOS RETORNADOS
