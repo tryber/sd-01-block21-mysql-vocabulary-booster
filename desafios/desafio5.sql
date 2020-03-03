aba24e0add77b78d08dbe954f28adef61504a090
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-03 18:33:29 -0300

    desafio 5

---
+++ desafios/desafio5.sql
@@ -0,0 +1,7 @@
+SELECT JOB_TITLE AS "Cargo",
+(MAX_SALARY - MIN_SALARY ) AS 'Variação Salarial',
+ROUND((MIN_SALARY /12),2) AS 'Média Mínima Mensal',
+ROUND((MAX_SALARY /12),2) AS 'Média Máxima Mensal'
+FROM hr.jobs
+GROUP BY JOB_TITLE
+ORDER BY (MAX_SALARY - MIN_SALARY);
