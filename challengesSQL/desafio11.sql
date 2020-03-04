3ae3f839ee0013009a680b0b19a20a7b2caa5928
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-03-04 13:29:58 -0300

    item 11 corrigido

--- challengesSQL/desafio11.sql
+++ challengesSQL/desafio11.sql
@@ -1,7 +1,9 @@
-SELECT CustomerID, ContactName AS Nome, Country AS País -- COUNT(*) AS Número_de_Compatriotas
-FROM w3schools.customers
--- GROUP BY Country
--- HAVING Número_de_Compatriotas > 1
-ORDER BY ContactName ASC;
-
--- REQUISITO NÃO FINALIZADO
+SELECT a.CustomerID, 
+a.ContactName AS Nome, 
+a.Country AS País, COUNT(a.Country) AS 'Número de Compatriotas'
+FROM w3schools.customers AS a
+INNER JOIN w3schools.customers AS c
+ON a.Country = c.Country
+WHERE a.CustomerID <> c.CustomerID
+GROUP BY a.CustomerID, a.Country
+ORDER BY a.ContactName ASC;
