125bc56367c389cd9ab8153290e5f20816b6691e
antoniopaolillo <antonio_francisco_p@live.com>
2020-03-04 18:14:05 -0300

    upando penultimos arquivos

---
+++ desafios/desafio18.sql
@@ -0,0 +1,14 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE FUNCTION ExibirQuantidadePessoasContratadasPorMesEAno(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE variable INT;
+	SELECT COUNT(*) FROM hr.employees WHERE YEAR(HIRE_DATE) = ano AND MONTH(HIRE_DATE) = mes INTO variable;
+	RETURN variable;
+END $$
+
+DELIMITER ;
+SELECT ExibirQuantidadePessoasContratadasPorMesEAno(6, 1987);
