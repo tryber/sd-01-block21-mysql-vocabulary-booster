c3c26e1956b4f3dcb522ce9e00b3f232dad42c84
dougfunny <douglaseduardo220483@gmail.com>
2020-03-04 18:57:08 -0300

    FINALMENTE FIZ A 6

--- desafios/desafio6.sql
+++ desafios/desafio6.sql
@@ -1,9 +1,16 @@
-SELECT employee_id, 
-(SELECT DEPARTMENT_ID, DEPARTMENT_NAME  AS 'Departamento'),
-(SELECT start_date FROM hr.job_history),  employee_id, 
-(SELECT CONCAT(FIRST_NAME, " ", LAST_NAME)AS 'Nome completo' FROM hr.employees),
-(SELECT JOB_ID, JOB_TITLE AS 'Cargo' FROM hr.jobs )
-FROM 
-
-
--- preso ainda na lógica...
+SELECT 
+	JH.START_DATE,
+	E.employee_id,
+	CONCAT(E.FIRST_NAME, " ", E.LAST_NAME) AS 'Nome completo',
+	J.JOB_ID,
+	J.JOB_TITLE AS 'Cargo',
+	D.DEPARTMENT_ID,
+	D.DEPARTMENT_NAME
+FROM hr.job_history AS JH
+INNER JOIN hr.employees AS E
+ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J
+ON JH.JOB_ID = J.JOB_ID
+INNER JOIN hr.departments AS D
+ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
+ORDER BY E.FIRST_NAME DESC;
