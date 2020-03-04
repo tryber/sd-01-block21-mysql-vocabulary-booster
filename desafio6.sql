fb98053712b2f29e5da44e41628dd8def59fdb91
vrramosm <vf.ramos7@gmail.com>
2020-03-04 13:49:08 -0300

    Refatorado o desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -5,11 +5,8 @@ SELECT JOBH.start_date,
   JOB.job_title AS 'Cargo',
   DEP.department_id,
   DEP.department_name AS Departamento
-FROM hr.employees AS EMP, 
-  hr.jobs AS JOB, 
-  hr.job_history AS JOBH, 
-  hr.departments AS DEP
-WHERE EMP.employee_id = JOBH.employee_id
-AND JOBH.job_id = JOB.job_id
-AND DEP.department_id = JOBH.department_id
-ORDER BY 3 DESC;     
+FROM hr.employees AS EMP
+INNER JOIN hr.job_history AS JOBH ON EMP.employee_id = JOBH.employee_id
+INNER JOIN hr.jobs AS JOB ON JOB.job_id = JOBH.job_id
+INNER JOIN hr.departments AS DEP ON DEP.department_id = JOBH.department_id
+ORDER BY 3 DESC;
