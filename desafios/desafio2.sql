b69c5001d544673b80d79ddd232984dc06373e49
antoniopaolillo <antonio_francisco_p@live.com>
2020-03-04 19:00:03 -0300

    cc

--- desafios/desafio2.sql
+++ desafios/desafio2.sql
@@ -1,7 +1,4 @@
 SELECT JOB_TITLE, 
-CASE WHEN max_salary > 4999 AND max_salary < 10001 THEN 'Baixo'
-	 WHEN max_salary > 10000 AND max_salary < 20001 THEN 'Médio'
-	 WHEN max_salary > 20000 AND max_salary < 30001 THEN 'Alto'
-     WHEN max_salary > 30000 THEN 'Altíssimo'
+CASE WHEN max_salary > 4999 AND max_salary < 10001 THEN 'Baixo' WHEN max_salary > 10000 AND max_salary < 20001 THEN 'Médio' WHEN max_salary > 20000 AND max_salary < 30001 THEN 'Alto' WHEN max_salary > 30000 THEN 'Altíssimo'
 END AS 'Nível'
 FROM hr.jobs;
