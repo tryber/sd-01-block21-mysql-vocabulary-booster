174f2b170ca86ff191f71f7771e27ac596609c53
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-03-06 13:46:07 -0300

    cc

--- desafio-=-4.sql
+++ desafio-=-4.sql
@@ -1,10 +1,10 @@
 SELECT JOB_ID as 'Código do Cargo', 
   avg(SALARY) as 'Média',
   CASE
-	WHEN avg(SALARY) >= 2000 and avg(SALARY) <= 5800 THEN 'Junior'
-	WHEN avg(SALARY) >= 5801 and avg(SALARY) <= 7500 THEN 'Pleno'
-	WHEN avg(SALARY) >= 7501 and avg(SALARY) <= 10500 THEN 'Senior'
-	WHEN avg(SALARY) > 10500 THEN 'CEO'
+  WHEN avg(SALARY) >= 2000 and avg(SALARY) <= 5800 THEN 'Junior'
+  WHEN avg(SALARY) >= 5801 and avg(SALARY) <= 7500 THEN 'Pleno'
+  WHEN avg(SALARY) >= 7501 and avg(SALARY) <= 10500 THEN 'Senior'
+  WHEN avg(SALARY) > 10500 THEN 'CEO'
   END AS Senioridade
 from hr.employees
 group by JOB_ID
