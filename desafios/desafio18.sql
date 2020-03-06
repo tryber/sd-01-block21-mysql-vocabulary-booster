cd373f1eb0a17c7da1b1fe224c8ebc55fc9c71b5
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-06 10:22:35 -0300

    18 blz

---
+++ desafios/desafio18.sql
@@ -0,0 +1,13 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION ExibirQuantidadePessoasContratadasPorMesEAno(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE RespostaDoTotalDeContratados INT;
+  SELECT COUNT(*) FROM hr.employees
+WHERE YEAR(HIRE_DATE) = ano AND MONTH(HIRE_DATE) = mes INTO RespostaDoTotalDeContratados;
+  RETURN RespostaDoTotalDeContratados;
+END $$
+DELIMITER ;
+
+SELECT ExibirQuantidadePessoasContratadasPorMesEAno(6, 1987);
