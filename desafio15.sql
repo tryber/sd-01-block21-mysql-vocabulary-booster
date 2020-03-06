081937e7597214b4d3db59e18fd4058d109ba324
vrramosm <vf.ramos7@gmail.com>
2020-03-06 09:40:04 -0300

    Consertando alguns erros de identaçao

--- desafio15.sql
+++ desafio15.sql
@@ -2,9 +2,9 @@ DELIMITER $$
 
 CREATE PROCEDURE BuscarMediaPorCategoriaDeTrabalho(IN id varchar(10))
 BEGIN
-	SELECT ROUND(AVG(salary), 2)
-  FROM hr.employees
-  WHERE job_id = id;
+SELECT ROUND(AVG(salary), 2)
+FROM hr.employees
+WHERE job_id = id;
 END 
 
 $$ DELIMITER ;
--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT EMP.employee_id,
-  UCASE(CONCAT(EMP.first_name,' ', EMP.last_name)) AS 'nome completo',
-  JOBH.start_date,
-  EMP.salary
+UCASE(CONCAT(EMP.first_name,' ', EMP.last_name)) AS 'nome completo',
+JOBH.start_date,
+EMP.salary
 FROM hr.employees AS EMP
 INNER JOIN hr.job_history AS JOBH ON EMP.employee_id = JOBH.employee_id
 WHERE MONTH(JOBH.start_date) IN (1, 2, 3)
