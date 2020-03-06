777768f8a9557357ea52e0082dfd4e6035ffd021
vrramosm <vf.ramos7@gmail.com>
2020-03-06 09:33:41 -0300

    Retirando tabs das queries

--- desafio18.sql
+++ desafio18.sql
@@ -3,13 +3,13 @@ DELIMITER $$
 CREATE FUNCTION ExibirQuantidadePessoasContratadasPorMesEAno(mes INT, ano INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total_funcionarios INT;
-  SELECT COUNT(*)
-  FROM hr.employees
-  WHERE YEAR(hire_date) = ano
-  AND MONTH(hire_date) = mes
-  INTO total_funcionarios;
-  RETURN total_funcionarios;
+DECLARE total_funcionarios INT;
+SELECT COUNT(*)
+FROM hr.employees
+WHERE YEAR(hire_date) = ano
+AND MONTH(hire_date) = mes
+INTO total_funcionarios;
+RETURN total_funcionarios;
 END
 
 $$ DELIMITER ;
--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
 SELECT JOBH.start_date, 
-  EMP.employee_id, 
-  CONCAT(EMP.first_name,' ', EMP.last_name) AS 'Nome Completo',
-  JOB.job_id,
-  JOB.job_title AS 'Cargo',
-  DEP.department_id,
-  DEP.department_name AS Departamento
+EMP.employee_id, 
+CONCAT(EMP.first_name,' ', EMP.last_name) AS 'Nome Completo',
+JOB.job_id,
+JOB.job_title AS 'Cargo',
+DEP.department_id,
+DEP.department_name AS Departamento
 FROM hr.employees AS EMP
 INNER JOIN hr.job_history AS JOBH ON EMP.employee_id = JOBH.employee_id
 INNER JOIN hr.jobs AS JOB ON JOB.job_id = JOBH.job_id
