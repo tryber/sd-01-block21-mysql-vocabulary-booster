462d752ff1cbe763f0d1d2b77a9e1d5fd4173c4d
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-06 10:26:37 -0300

    CC

--- desafios/desafio15.sql
+++ desafios/desafio15.sql
@@ -1,9 +1,13 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE BuscarMediaPorCategoriaDeTrabalho ( IN ValorRecebido VARCHAR(100))
+CREATE PROCEDURE BuscarMediaPorCategoriaDeTrabalho ( IN ValorRecebido VARCHAR
+(100))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) AS Média FROM hr.employees
-	WHERE JOB_ID = ValorRecebido;
-END $$
+  SELECT ROUND(AVG(SALARY), 2) AS Média
+  FROM hr.employees
+  WHERE JOB_ID = ValorRecebido;
+END
+$$
 DELIMITER ;
-call BuscarMediaPorCategoriaDeTrabalho('IT_PROG');
+call BuscarMediaPorCategoriaDeTrabalho
+('IT_PROG');
