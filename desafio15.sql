96cbf4989b1043594f3321f11115c62d5e55e8b5
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-03-06 11:29:21 -0300

    code climate 1

--- desafio15.sql
+++ desafio15.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 
 CREATE PROCEDURE BuscarMediaPorCategoriaDeTrabalho(IN id VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY),2) AS media
-    FROM hr.employees AS e
-    WHERE e.job_id = id
-    GROUP BY e.job_id;
+SELECT ROUND(AVG(e.SALARY),2) AS media
+FROM hr.employees AS e
+WHERE e.job_id = id
+GROUP BY e.job_id;
 END $$
 
 DELIMITER ;
