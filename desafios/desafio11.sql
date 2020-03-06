f76ef7daccf8fc1ab4fe57edf16b8af03b102e0d
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-06 10:37:33 -0300

    será que agora vai?

--- desafios/desafio11.sql
+++ desafios/desafio11.sql
@@ -1,12 +1,11 @@
 SELECT
-C.CustomerID,
-C.ContactName AS Nome,
-C.Country AS PAÍS,
-COUNT(B.Country) AS 'Número de Compatriotas'
+  C.CustomerID,
+  C.ContactName AS Nome,
+  C.Country AS PAÍS,
+  COUNT(B.Country) AS 'Número de Compatriotas'
 FROM w3schools.customers AS C
-INNER JOIN w3schools.customers AS B
-ON  C.Country = B.Country
+  INNER JOIN w3schools.customers AS B
+  ON  C.Country = B.Country
 WHERE C.CustomerID != B.CustomerID
 GROUP BY 1
 ORDER BY 2;
- 
