19837e2b2564bb8e82b3c39b08dbe17f283c0c17
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-06 10:36:02 -0300

    agora foi. passei no cc

--- desafios/desafio11.sql
+++ desafios/desafio11.sql
@@ -1,27 +1,12 @@
 SELECT
-  C.CustomerID,
-  C.ContactName AS Nome,
-  C.Country AS PAÍS,
-  COUNT(B.Country) AS 'Número de Compatriotas'
+C.CustomerID,
+C.ContactName AS Nome,
+C.Country AS PAÍS,
+COUNT(B.Country) AS 'Número de Compatriotas'
 FROM w3schools.customers AS C
-  INNER JOIN w3schools.customers AS B
-  ON  C.Country = B.Country
+INNER JOIN w3schools.customers AS B
+ON  C.Country = B.Country
 WHERE C.CustomerID != B.CustomerID
 GROUP BY 1
 ORDER BY 2;
-
--- EXPLICA PORQUE FEZ O COUNT PELO PK E COLOCAR DOIS PARAMETROS NO GROUP BY
-
--- SELECT 
--- C.CustomerID,
--- C.ContactName AS Nome,
--- C.Country AS PAÍS,
--- COUNT(B.CustomerID) AS 'Número de Compatriotas'
--- FROM w3schools.customers AS C
--- INNER JOIN w3schools.customers AS B
--- ON C.Country = B.Country
--- WHERE C.CustomerID != B.CustomerID 
--- GROUP BY 1, 3
--- ORDER BY 2;
-
-  
+ 
