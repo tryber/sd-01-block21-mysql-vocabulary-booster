590857855b8a6415cddb0bfdc586ababa9ff4165
vrramosm <vf.ramos7@gmail.com>
2020-03-07 14:12:00 -0300

    Corrigido os desafios 3 e 5

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,3 @@
 SELECT job_title, (max_salary - min_salary) AS 'Diferença média entre salários mínimos e máximos' 
 FROM hr.jobs 
-GROUP BY job_title
 ORDER BY 2;
--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,6 @@
 SELECT job_title AS 'Cargo', 
 (max_salary - min_salary) AS 'Variação Salarial',
-ROUND(AVG(min_salary/12), 2) AS 'Média Mínima Mensal',
-ROUND(AVG(max_salary/12), 2) AS 'Média Máxima Mensal'
+ROUND(min_salary / 12, 2) AS 'Média Mínima Mensal',
+ROUND(max_salary / 12, 2) AS 'Média Máxima Mensal'
 FROM hr.jobs
-GROUP BY job_title
 ORDER BY 2;
