4f4bf013e1bc94cca8299f49c70098c724d971f0
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-03-08 14:51:17 -0300

    cc

--- desafio-=-10.sql
+++ desafio-=-10.sql
@@ -1,7 +1,7 @@
 select ProductID, 
-	MIN(Quantity) as Mínimo,
-	MAX(Quantity) as Máximo,
-    round(AVG(Quantity), 2) as Média
+  MIN(Quantity) as Mínimo,
+  MAX(Quantity) as Máximo,
+  round(AVG(Quantity), 2) as Média
 from w3schools.order_details
 group by ProductID
 HAVING Média > 20
